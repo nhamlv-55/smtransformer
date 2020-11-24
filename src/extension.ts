7e7cd53858aa02c5f5aa799a09028337dfc2c225
Nham Le <nv3le@uwaterloo.ca>
2020-11-24 02:52:01 -0500

    add openEditor and squashNegation

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {AST, ASTNode, ASTTransformer} from './transformers';
+const {uuid} = require('uuidv4');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -10,6 +11,16 @@ function getRange(textEditor){
 	var lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
 	var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
 	return textRange;
+}
+
+function getSelectedNode(ast){
+	const editor = vscode.window.activeTextEditor;
+
+	const position = editor?.selection.active;
+
+	let selectedNode = ast.findNode(position!.line, position!.character);
+	console.log(selectedNode);
+	return selectedNode;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -28,32 +39,59 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from smtransformer!');
 	});
 
+	vscode.commands.registerCommand('smtransformer.openEditor', () => {
+		const editor = vscode.window.activeTextEditor;
+		const selectedText = editor!.document.getText(editor!.selection);
+		var setting: vscode.Uri = vscode.Uri.parse("untitled:"+uuid()+".txt");
+		console.log(setting);
+		vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
+			vscode.window.showTextDocument(a, 1, false).then(e => {
+				e.edit(edit => {
+					edit.insert(new vscode.Position(0, 0), selectedText);
+				});
+			});
+		}, (error: any) => {
+			console.error(error);
+			debugger;
+		});
+	})
+
+
 	vscode.commands.registerCommand('smtransformer.toImp', ()=>{
 		const editor = vscode.window.activeTextEditor;
 		const fullText = editor?.document.getText()!;
-		console.log(fullText);
-
-		const position = editor?.selection.active;
-		console.log("position", position);
 		let originalAst = new AST(fullText);
-		console.log(originalAst);
 
-		let selectedNode = originalAst.findNode(position!.line, position!.character);
-		console.log(selectedNode);
+		let selectedNode = getSelectedNode(originalAst);
 		if(selectedNode){
 			let res: AST;
 			let dirty: boolean;
 			[dirty, res] = transformer.toImp(selectedNode, originalAst, {}, "true");
 			if(dirty){
-				var lastLine = editor!.document.lineCount+1;
-				console.log("last line", lastLine);
 				editor?.edit(edit => {edit.replace(getRange(editor), res.toString(-1, res.nodeList[0]))});
 			}
 			console.log(res.toString(-1, res.nodeList[0]));
 		}
 		vscode.window.showInformationMessage('To Imp from smtransformer!');
 	});
+	
+	vscode.commands.registerCommand('smtransformer.squashNegation', ()=>{
+		const editor = vscode.window.activeTextEditor;
+		const fullText = editor?.document.getText()!;
+		let originalAst = new AST(fullText);
 
+		let selectedNode = getSelectedNode(originalAst);
+		if(selectedNode){
+			let res: AST;
+			let dirty: boolean;
+			[dirty, res] = transformer.squashNegation(selectedNode, originalAst, {}, "true");
+			if(dirty){
+				editor?.edit(edit => {edit.replace(getRange(editor), res.toString(-1, res.nodeList[0]))});
+			}
+			console.log(res.toString(-1, res.nodeList[0]));
+		}
+		vscode.window.showInformationMessage('squashNegation from smtransformer!');
+	});
 }
 
 // this method is called when your extension is deactivated
